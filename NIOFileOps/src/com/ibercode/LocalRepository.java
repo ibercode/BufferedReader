e84a63cd9ef8feea7c9a512668adb06ecf6458dc
ibercode <31028993+ibercode@users.noreply.github.com>
2018-08-20 12:02:26 +0200

    BufferedWriter Implemented

--- NIOFileOps/src/com/ibercode/LocalRepository.java
+++ NIOFileOps/src/com/ibercode/LocalRepository.java
@@ -1,8 +1,12 @@
 package com.ibercode;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.util.function.Consumer;
 import java.util.stream.Collectors;
 
 public class LocalRepository {
@@ -10,13 +14,24 @@ public class LocalRepository {
 	public void showMovies() throws IOException {
 		
 		BufferedReader bufferReader = new BufferedReader(new FileReader("C:\\moviesDB.txt"));
+		BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(new File("outputLocal.txt")));
+		
+		Consumer<String> writeLine = line -> {
+			try {
+				bufferedWriter.write(line);
+				bufferedWriter.newLine();
+				System.out.println(line);
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		};
 		
 		bufferReader.lines()
 			.collect(Collectors.toList())
 			.stream()
-			.limit(10)
-			.forEach(System.out::println);
+			.forEach(writeLine);
 		
 		bufferReader.close();
+		bufferedWriter.close();
 	}
 }
--- NIOFileOps/src/com/ibercode/URLRepository.java
+++ NIOFileOps/src/com/ibercode/URLRepository.java
@@ -1,10 +1,14 @@
 package com.ibercode;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.URL;
 import java.nio.charset.StandardCharsets;
+import java.util.function.Consumer;
 import java.util.stream.Collectors;
 
 public class URLRepository {
@@ -15,13 +19,24 @@ public class URLRepository {
 		
 		BufferedReader bufferReader = new BufferedReader(
 				new InputStreamReader(url.openStream(), StandardCharsets.UTF_8));
-
+		BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(new File("outputURL.txt")));
+		
+		Consumer<String> writeLine = line -> {
+			try {
+				bufferedWriter.write(line);
+				bufferedWriter.newLine();
+				System.out.println(line);
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		};
+		
 		bufferReader.lines()
 			.collect(Collectors.toList())
 			.stream()
-			.limit(10)
-			.forEach(System.out::println);
+			.forEach(writeLine);
 
 		bufferReader.close();
+		bufferedWriter.close();
 	}
 }
